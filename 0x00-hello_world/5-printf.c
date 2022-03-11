6f4a2824d30ee17bcf9704d68c3efd2d5f0ff0da
Maurice Otieno <yawasmaurice@gmail.com>
2022-03-10 20:09:46 -0800

    task 5 hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - prints the phrase with proper grammar, but the outcome is a piece of art
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"with proper grammar, but the outcomeis a piece of art");
+	return (0);
+}
