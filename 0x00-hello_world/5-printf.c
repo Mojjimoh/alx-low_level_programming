e902c4ae23ace0a36bc324437357b05b04066a06
Maurice Otieno <yawasmaurice@gmail.com>
2022-03-10 20:24:14 -0800

    Fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main - prints the phrase with proper grammar, but the outcome is a piece of art
- * Return: Always 0 (success)
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
