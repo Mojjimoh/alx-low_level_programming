30397f99ba7ef2687c37a91b5f1af02630034cc9
Maurice Otieno <yawasmaurice@gmail.com>
2022-03-10 19:48:14 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/*
+ * main - prints the phrase programming is like buiding a multilingual puzzle
+ * return: Always 0 (success)
+ */
+{
+	int main(void)
+		puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
