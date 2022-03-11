9f66abac87e5aa05ee68cc91fa9614d2d9c51408
Maurice Otieno <yawasmaurice@gmail.com>
2022-03-10 19:55:19 -0800

    Refix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  * main - prints the phrase programming is like buiding a multilingual puzzle
  * return: Always 0 (success)
  */
+int main(void)
 {
-	int main(void)
-		puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
